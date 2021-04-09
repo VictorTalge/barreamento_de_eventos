2f1755c00730a7b6968cb400e901daa03f93bdf9
Victor <vt.talge@gmail.com>
2021-04-08 21:22:16 -0300

    Primeiro

---
+++ src/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+//para enviar eventos para os demais microsserviços
+const axios = require('axios');
+const app = express();
+app.use(express.json());
+app.post("/eventos", (req, res) => {
+    console.log(req.body);
+    res.status(200).send({
+        msg: "ok"
+    });
+});
+app.listen(10000, () => {
+    console.log('Barramento de eventos. Porta 10000.')
+})
